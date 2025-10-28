a3f557e8e8e0fc867284cbc1e481b628b5452b1d
sinduke <sinduke@outlook.com>
2025-10-29 00:10:41 +0800

    添加SwiftLint脚本 运行的沙箱环境 和简单自定义配置

--- AIChatsUITests/AIChatsUITestsLaunchTests.swift
+++ AIChatsUITests/AIChatsUITestsLaunchTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class AIChatsUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
