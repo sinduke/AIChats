f669097681b6d1640621a0611ba137f391856bd3
sinduke <sinduke@outlook.com>
2025-10-28 18:25:38 +0800

    Initial Commit

---
+++ AIChats/AIChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatsApp.swift
+//  AIChats
+//
+//  Created by sinduke on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatsTests/AIChatsTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatsTests.swift
+//  AIChatsTests
+//
+//  Created by sinduke on 10/28/25.
+//
+
+import Testing
+
+struct AIChatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
