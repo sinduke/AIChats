0085766133d2bb354443b65d9e4084cf57242b93
sinduke <sinduke@outlook.com>
2025-10-31 14:03:00 +0800

    添加HeroCellView

---
+++ AIChats/Components/Views/HeroCellView.swift
@@ -0,0 +1,65 @@
+//
+//  HeroCellView.swift
+//  AIChats
+//
+//  Created by sinduke on 10/31/25.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    var title: String? = "This is a Title"
+    var subtitle: String? = "This is a subtitle for the hero cell view."
+    var image: String? = Constants.randomImageURL
+    var body: some View {
+        ZStack {
+            if let image {
+                ImageLoadView(urlString: image)
+            } else {
+                Rectangle()
+                    .fill(.gray.opacity(0.3))
+            }
+        }
+        .overlay(alignment: .bottomLeading, content: {
+            VStack(alignment: .leading, spacing: 4) {
+                if let title {
+                    Text(title)
+                        .font(.headline)
+                }
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.subheadline)
+                }
+            }
+            .foregroundStyle(.white)
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(
+                LinearGradient(colors: [
+                    .black.opacity(0.1),
+                    .black.opacity(0.3),
+                    .black.opacity(0.4)
+                ], startPoint: .top, endPoint: .bottom)
+            )
+        })
+        .clipShape(.rect(cornerRadius: 16, style: .continuous))
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack {
+            HeroCellView()
+                .frame(width: 300, height: 200)
+            HeroCellView()
+                .frame(width: 300, height: 400)
+            HeroCellView(image: nil)
+                .frame(width: 300, height: 200)
+            HeroCellView(title: nil)
+                .frame(width: 300, height: 200)
+            HeroCellView(subtitle: nil)
+                .frame(width: 300, height: 200)
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
